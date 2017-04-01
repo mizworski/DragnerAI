2309b1c2b0b83256d284046a02ba82790d58a32f
Michał Izworski <m.izworski7@gmail.com>
2017-04-02 00:12:20 +0200

    Opus library removed due to errors on remote.

--- DragnerAI/music_player.py
+++ DragnerAI/music_player.py
@@ -2,13 +2,14 @@ import asyncio
 import discord
 from discord.ext import commands
 
-if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
+
+# if not discord.opus.is_loaded():
+#     # the 'opus' library here is opus.dll on windows
+#     # or libopus.so on linux in the current directory
+#     # you should replace this with the location the
+#     # opus library is located in and with the proper filename.
+#     # note that on windows this DLL is automatically provided for you
+#     discord.opus.load_opus('opus')
 
 
 class VoiceEntry:
